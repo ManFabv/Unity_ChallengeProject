057b74a83375f5c0485dd89a44ea99f187485ec8
Fabricio Manrique <fabricio.manrique@bechtle.com>
2020-05-17 01:43:03 -0300

    Added summary to CD/CI

--- Assets/Editor/Build/BuildAndroid.cs
+++ Assets/Editor/Build/BuildAndroid.cs
@@ -1,31 +1,40 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-namespace PPop.Editor.Build
+public class BuildAndroid : MonoBehaviour 
 {
-    public class BuildAndroid : MonoBehaviour 
+    [MenuItem("Build/Build Android")]
+    public static void ExportAndroidStudioProject()
     {
-        [MenuItem("Build/Build Android")]
-        public static void ExportAndroidStudioProject()
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+
+        EditorUserBuildSettings.symlinkLibraries = true;
+        EditorUserBuildSettings.development = true;
+        EditorUserBuildSettings.allowDebugging = true;
+        EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
+
+        List<string> scenes = new List<string>();
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
         {
-            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
-
-            EditorUserBuildSettings.symlinkLibraries = true;
-            EditorUserBuildSettings.development = true;
-            EditorUserBuildSettings.allowDebugging = true;
-            EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
-
-            List<string> scenes = new List<string>();
-            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            if (EditorBuildSettings.scenes[i].enabled)
             {
-                if (EditorBuildSettings.scenes[i].enabled)
-                {
-                    scenes.Add(EditorBuildSettings.scenes[i].path);
-                }
+                scenes.Add(EditorBuildSettings.scenes[i].path);
             }
-
-            BuildPipeline.BuildPlayer(scenes.ToArray(), "Build\\Android\\PPOP_ChallengeProject", BuildTarget.Android, BuildOptions.None);
         }
+
+        BuildReport report = BuildPipeline.BuildPlayer(scenes.ToArray(), "Build\\Android\\PPOP_ChallengeProject", BuildTarget.Android, BuildOptions.None);
+
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        else if (summary.result == BuildResult.Failed)
+            Debug.Log("Build Failed!");
+
+        Debug.Log("Errors: " + summary.totalErrors);
+        Debug.Log("Warnings: " + summary.totalWarnings);
+        Debug.Log("Total elapsed time: " + summary.totalTime);
     }
 }
--- Assets/Editor/Build/BuildiOS.cs
+++ Assets/Editor/Build/BuildiOS.cs
@@ -1,30 +1,39 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-namespace PPop.Editor.Build
+public class BuildiOS : MonoBehaviour 
 {
-    public class BuildiOS : MonoBehaviour
+    [MenuItem("Build/Build iOS")]
+    public static void ExportXcodeProject()
     {
-        [MenuItem("Build/Build iOS")]
-        public static void ExportXcodeProject()
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.iOS);
+
+        EditorUserBuildSettings.symlinkLibraries = true;
+        EditorUserBuildSettings.development = true;
+        EditorUserBuildSettings.allowDebugging = true;
+
+        List<string> scenes = new List<string>();
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
         {
-            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.iOS);
-
-            EditorUserBuildSettings.symlinkLibraries = true;
-            EditorUserBuildSettings.development = true;
-            EditorUserBuildSettings.allowDebugging = true;
-
-            List<string> scenes = new List<string>();
-            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            if (EditorBuildSettings.scenes[i].enabled)
             {
-                if (EditorBuildSettings.scenes[i].enabled)
-                {
-                    scenes.Add(EditorBuildSettings.scenes[i].path);
-                }
+                scenes.Add(EditorBuildSettings.scenes[i].path);
             }
-
-            BuildPipeline.BuildPlayer(scenes.ToArray(), "Build\\iOS\\PPOP_ChallengeProject", BuildTarget.iOS, BuildOptions.None);
         }
+
+        BuildReport report = BuildPipeline.BuildPlayer(scenes.ToArray(), "Build\\iOS\\PPOP_ChallengeProject", BuildTarget.iOS, BuildOptions.None);
+
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        else if (summary.result == BuildResult.Failed)
+            Debug.Log("Build Failed!");
+
+        Debug.Log("Errors: " + summary.totalErrors);
+        Debug.Log("Warnings: " + summary.totalWarnings);
+        Debug.Log("Total elapsed time: " + summary.totalTime);
     }
 }
